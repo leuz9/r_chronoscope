30f7e1e6e7d338c5074384d7caa36b5c1f95b22c
Roland Niokhor Faye <leuzleuz9@gmail.com>
2025-01-09 11:38:09 +0000

    verif

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -18,7 +18,7 @@ export default function Sidebar() {
     <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
       <div className="flex-1 flex flex-col min-h-0 bg-gray-800 dark:bg-gray-900">
         <div className="flex items-center flex-shrink-0 px-4 py-5">
-          <h1 className="text-xl font-bold text-white">Chronoscope</h1>
+          <h1 className="text-xl font-bold text-white">ChronoscopeR</h1>
         </div>
         <div className="flex-1 flex flex-col justify-between">
           <nav className="flex-1 px-2 space-y-1">
